a3edfb9080aadbfc5afe9f080fa4b2c585564d83
1400003 <1400003@rgu.ac.uk>
2015-10-19 16:36:35 +0100

    Last

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
             <h1> It's ALIVE!!!  </h1>
             <p>
                 <?php
-                    echo "Hello WORLD";
-                    echo "Hello, " . " " . "World" . "!";
+                    echo " Hello WORLD ";
+                    echo " Hello, " . " " . " World " . " ! ";
                     echo 5 * 7;
-                    $myname = "Frodo Baggins";
+                    $myname = " Frodo Baggins ";
                     $myage = 111;
-                    echo "My name is" . $myname . "and I am " . $myage;
+                    echo " My name is " . $myname . " and I am " . $myage;
                 ?>
             </p>
             <h1> Hi </h1><hr><br>
