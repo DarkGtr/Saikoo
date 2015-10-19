537f5041a4ac93f63d961985ef69b7d33f9fc72c
1400003 <1400003@rgu.ac.uk>
2015-10-19 16:01:17 +0100

    Added about ROCKO LEE

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 
 <!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>Deployment  Test</title>
-</head>
-<body>
-<h1> It's ALIVE!!!  </h1>
-<h1> Yo Yo </h1>
-<h1> ROCKO LEE </h1>
-</body>
-</html>
+    <html>
+        <head lang="en">
+            <meta charset="UTF-8">
+            <title>Deployment  Test</title>
+        </head>
+        <body>
+            <h1> It's ALIVE!!!  </h1>
+            <p>
+                <?php
+                    echo "Hello WORLD";
+                ?>
+            </p>
+        </body>
+    </html>
