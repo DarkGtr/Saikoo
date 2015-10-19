367d9edaa110d11470eebc638ae547d97a402ded
1400003 <1400003@rgu.ac.uk>
2015-10-19 16:39:19 +0100

    Last

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
                     $myname = " Frodo Baggins ";
                     $myage = 111;
                     echo " My name is " . $myname . " and I am " . $myage;
+                    echo " Hey ";
                 ?>
             </p>
             <h1> Hi </h1><hr><br>
