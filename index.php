f6f457291388af7aa79fa7d9d3ff06309051f140
1400003 <1400003@rgu.ac.uk>
2015-10-19 16:48:21 +0100

    Last

--- index.php
+++ index.php
@@ -14,10 +14,12 @@
                     echo 5 * 7;
                     $myname = " Frodo Baggins ";
                     $myage = 111;
+                    $mynamesaad = " Saad Alm ";
                     echo " My name is " . $myname . " and I am " . $myage;
                     echo " Hey ";
+                    echo " Atashi wa " . $mynamesaad . " desu ";
                 ?>
             </p>
-            <h1> Hi </h1><hr><br>
-        </body>
+            <h1> Hi </h1>
+        </body><hr><br>
     </html>
