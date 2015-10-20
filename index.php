5e3db9694c76e42ebd92cee1a40cdfe36dc55cce
1400003 <1400003@rgu.ac.uk>
2015-10-20 16:24:55 +0100

    Last

--- index.php
+++ index.php
@@ -69,15 +69,15 @@
                     $wantedgood = "mugs";
                 SWITCH ($wantedgood) {
                     CASE 1:
-                        IF ($wantedgood == "mugs");
+                        IF ($wantedgood = "mugs");
                         print "You have to be 18 to buy mugs";
                         BREAK;
                     CASE 2:
-                        IF ($wantedgood == "specs");
+                        IF ($wantedgood = "specs");
                         print "You have to be 16 to buy specs";
                         BREAK;
                     CASE 3:
-                        IF ($wantedgood == "Sausages Rolls");
+                        IF ($wantedgood = "Sausages Rolls");
                         print "You have to be over 21 to buy Sausages Rolls";
                         BREAK;
                 }
