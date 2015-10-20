5196dfe621e91bf6962b588d52f6a1b0f29ec50e
1400003 <1400003@rgu.ac.uk>
2015-10-20 17:29:55 +0100

    Last

--- index.php
+++ index.php
@@ -91,7 +91,7 @@
                     print($myarray[2]);
                 ?>
                 <iframe width="420" height="315"
-                        src="http://www.youtube.com/watch?v=5GUPtoiFm5A">
+                        src="http://www.youtube.com/embed/?v=5GUPtoiFm5A">
                 </iframe>
             </div>
         </body><hr><br>
