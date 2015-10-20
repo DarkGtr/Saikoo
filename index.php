bc3c89787bc2c88ce84de9f58363bd55cf957408
1400003 <1400003@rgu.ac.uk>
2015-10-20 17:27:52 +0100

    Last

--- index.php
+++ index.php
@@ -90,10 +90,9 @@
                     ECHO $myarray[1];
                     print($myarray[2]);
                 ?>
-                <video width="1270" controls>
-                    <source src="https://www.youtube.com/watch?v=5GUPtoiFm5A" type="video/flv">
-                    Your browser does not support html5 video
-                </video>
+                <iframe width="1280" height="720"
+                        src="https://www.youtube.com/watch?v=5GUPtoiFm5A">
+                </iframe>
             </div>
         </body><hr><br>
     </html>
